b3b3375f6bc01990c978ab549e3618c94a578597
BLayus <93221753+BLayus@users.noreply.github.com>
2021-11-16 12:10:07 -0300

    Delete Lira Desafio SQL  1 Q5.sql

--- Lira Desafio SQL  1 Q5.sql
+++
@@ -1,6 +0,0 @@
-/* encontre os clientes do estado de SP e que não morem na cidade de São Paulo */
-
-SELECT *
-    FROM  `global-course-329623.Olist.olist_customers_dataset`
-    WHERE customer_state = 'SP'
-    AND customer_city <> 'sao paulo'
