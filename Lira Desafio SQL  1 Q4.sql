c081530a585eaace2d2d9600edd1d26c79f7bb24
BLayus <93221753+BLayus@users.noreply.github.com>
2021-11-16 12:10:00 -0300

    Delete Lira Desafio SQL  1 Q4.sql

--- Lira Desafio SQL  1 Q4.sql
+++
@@ -1,8 +0,0 @@
-/* retorne pedidos que foram feitos somente no ano de 2017. */
-
-SELECT 
-order_id AS Pedidos,
-EXTRACT(YEAR FROM order_purchase_timestamp) AS YEAR,
-FROM `global-course-329623.Olist.olist_orders_dataset` 
-GROUP BY YEAR, Pedidos
-HAVING YEAR = 2017
